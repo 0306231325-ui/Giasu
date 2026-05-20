8a4aff0337037c41049866655e75d2710ed5629e
phu <0306231325@caothang.edu.vn>
2026-05-20 15:17:16 +0700

    cap nhat db

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,7 +1,11 @@
 <?php
-
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/test-db', function () {
+    return DB::select("SHOW TABLES");
 });
